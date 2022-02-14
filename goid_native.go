5202f290d2343e8621a008be1dd52279099e9765
TimAndy <xuchonglei@126.com>
2022-02-14 16:44:28 +0800

    Rename go:linkname's local name same as remote name

--- goid_native.go
+++ goid_native.go
@@ -9,43 +9,42 @@ const (
 	gDead = 6
 )
 
-//go:linkname runtimeG runtime.g
-type runtimeG struct {
+//go:linkname g runtime.g
+type g struct {
 }
 
-//go:linkname runtimeMutex runtime.mutex
-type runtimeMutex struct {
+//go:linkname mutex runtime.mutex
+type mutex struct {
 }
 
-//go:linkname runtimeAllgs runtime.allgs
-var runtimeAllgs []*runtimeG
+//go:linkname allgs runtime.allgs
+var allgs []*g
 
-//go:linkname runtimeAllglock runtime.allglock
-var runtimeAllglock runtimeMutex
+//go:linkname allglock runtime.allglock
+var allglock mutex
 
-//go:linkname runtimeLock runtime.lock
-func runtimeLock(l *runtimeMutex)
+//go:linkname lock runtime.lock
+func lock(l *mutex)
 
-//go:linkname runtimeUnlock runtime.unlock
-func runtimeUnlock(l *runtimeMutex)
+//go:linkname unlock runtime.unlock
+func unlock(l *mutex)
 
-//go:linkname runtimeReadgstatus runtime.readgstatus
-func runtimeReadgstatus(g *runtimeG) uint32
+//go:linkname readgstatus runtime.readgstatus
+func readgstatus(gp *g) uint32
 
-//go:linkname runtimeIsSystemGoroutine runtime.isSystemGoroutine
-func runtimeIsSystemGoroutine(gp *runtimeG, fixed bool) bool
+//go:linkname isSystemGoroutine runtime.isSystemGoroutine
+func isSystemGoroutine(gp *g, fixed bool) bool
 
 // getAllGoidByNative retrieve all goid through runtime.allgs
 func getAllGoidByNative() ([]int64, bool) {
 	defer func() {
 		recover()
 	}()
-	runtimeLock(&runtimeAllglock)
-	defer runtimeUnlock(&runtimeAllglock)
-	allgs := runtimeAllgs
+	lock(&allglock)
+	defer unlock(&allglock)
 	goids := make([]int64, 0, len(allgs))
 	for _, gp := range allgs {
-		if runtimeReadgstatus(gp) == gDead || runtimeIsSystemGoroutine(gp, false) {
+		if readgstatus(gp) == gDead || isSystemGoroutine(gp, false) {
 			continue
 		}
 		goid := findGoidPointer(unsafe.Pointer(gp))
@@ -59,11 +58,10 @@ func getAllGoidByNative() ([]int64, bool) {
 
 // foreachGoidByNative run a func for each goroutine's goid through runtime.allgs
 func foreachGoidByNative(fun func(goid int64)) bool {
-	runtimeLock(&runtimeAllglock)
-	defer runtimeUnlock(&runtimeAllglock)
-	allgs := runtimeAllgs
+	lock(&allglock)
+	defer unlock(&allglock)
 	for _, gp := range allgs {
-		if runtimeReadgstatus(gp) == gDead || runtimeIsSystemGoroutine(gp, false) {
+		if readgstatus(gp) == gDead || isSystemGoroutine(gp, false) {
 			continue
 		}
 		goid := findGoidPointer(unsafe.Pointer(gp))
