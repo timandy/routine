6bfa4ae240bd910e53cb302e408b653842517347
TimAndy <xuchonglei@126.com>
2022-03-04 17:55:00 +0800

    Improve memory safety by adding magic numbers in thread struct

--- thread.go
+++ thread.go
@@ -2,7 +2,16 @@ package routine
 
 import "unsafe"
 
+const threadMagic = int64('r')<<48 |
+	int64('o')<<40 |
+	int64('u')<<32 |
+	int64('t')<<24 |
+	int64('i')<<16 |
+	int64('n')<<8 |
+	int64('e')
+
 type thread struct {
+	magic                   int64
 	id                      int64
 	threadLocals            *threadLocalMap
 	inheritableThreadLocals *threadLocalMap
@@ -14,7 +23,7 @@ func currentThread(create bool) *thread {
 	//nothing inherited
 	if label == nil {
 		if create {
-			newt := &thread{id: goid}
+			newt := &thread{magic: threadMagic, id: goid}
 			setProfLabel(unsafe.Pointer(newt))
 			return newt
 		}
@@ -22,9 +31,9 @@ func currentThread(create bool) *thread {
 	}
 	//inherited need recreate
 	t := (*thread)(label)
-	if t.id != goid {
+	if t.id != goid || t.magic != threadMagic {
 		if create {
-			newt := &thread{id: goid}
+			newt := &thread{magic: threadMagic, id: goid}
 			setProfLabel(unsafe.Pointer(newt))
 			return newt
 		}
