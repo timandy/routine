5c5f42a498b6841a4fb06cdd557392c44915eee4
TimAndy <xuchonglei@126.com>
2022-03-08 20:10:41 +0800

    Add testcase for traceStack() method

--- stack_test.go
+++ stack_test.go
@@ -12,3 +12,22 @@ func TestTraceStack(t *testing.T) {
 	assert.True(t, bytes.HasPrefix(stack, []byte("goroutine ")))
 	assert.False(t, bytes.HasSuffix(stack, []byte("\n")))
 }
+
+func TestTraceStackDeep(t *testing.T) {
+	stack := traceStackDeep(20)
+	assert.Greater(t, len(stack), 1024)
+	assert.True(t, bytes.HasPrefix(stack, []byte("goroutine ")))
+	assert.False(t, bytes.HasSuffix(stack, []byte("\n")))
+}
+
+func traceStackDeep(deep int) []byte {
+	return traceStackDeepRecursive(1, deep)
+}
+
+func traceStackDeepRecursive(cur int, tar int) []byte {
+	if cur < tar {
+		cur++
+		return traceStackDeepRecursive(cur, tar)
+	}
+	return traceStack()
+}
