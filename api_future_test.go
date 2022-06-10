7b611d07e9f561d34761df6ced9bd20773c953dc
TimAndy <xuchonglei@126.com>
2022-06-10 15:04:47 +0800

    Rename Feature to Future because of spell error

--- api_future_test.go
+++ api_future_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 )
 
-func TestNewFeature(t *testing.T) {
-	fea := NewFeature()
+func TestNewFuture(t *testing.T) {
+	fea := NewFuture()
 	assert.NotNil(t, fea)
 	//
-	p, ok := fea.(*feature)
+	p, ok := fea.(*future)
 	assert.Same(t, p, fea)
 	assert.True(t, ok)
 }
--- future_test.go
+++ future_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestFuture_Complete(t *testing.T) {
-	fea := NewFeature()
+	fea := NewFuture()
 	go func() {
 		fea.Complete(1)
 	}()
@@ -27,23 +27,23 @@ func TestFuture_CompleteError_Common(t *testing.T) {
 			assert.Equal(t, "RuntimeError: 1", line)
 			//
 			line = lines[1]
-			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.(*feature).CompleteError() in "))
-			assert.True(t, strings.HasSuffix(line, "feature.go:20"))
+			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.(*future).CompleteError() in "))
+			assert.True(t, strings.HasSuffix(line, "future.go:20"))
 			//
 			line = lines[2]
 			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.TestFuture_CompleteError_Common."))
-			assert.True(t, strings.HasSuffix(line, "feature_test.go:50"))
+			assert.True(t, strings.HasSuffix(line, "future_test.go:50"))
 			//
 			line = lines[3]
 			assert.True(t, strings.HasPrefix(line, "   at runtime.gopanic() in "))
 			//
 			line = lines[4]
 			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.TestFuture_CompleteError_Common."))
-			assert.True(t, strings.HasSuffix(line, "feature_test.go:53"))
+			assert.True(t, strings.HasSuffix(line, "future_test.go:53"))
 		}
 	}()
 	//
-	fea := NewFeature()
+	fea := NewFuture()
 	go func() {
 		defer func() {
 			if cause := recover(); cause != nil {
@@ -69,18 +69,18 @@ func TestFuture_CompleteError_RuntimeError(t *testing.T) {
 			//
 			line = lines[1]
 			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.TestFuture_CompleteError_RuntimeError."))
-			assert.True(t, strings.HasSuffix(line, "feature_test.go:87"))
+			assert.True(t, strings.HasSuffix(line, "future_test.go:87"))
 			//
 			line = lines[2]
 			assert.True(t, strings.HasPrefix(line, "   at runtime.gopanic() in "))
 			//
 			line = lines[3]
 			assert.True(t, strings.HasPrefix(line, "   at github.com/timandy/routine.TestFuture_CompleteError_RuntimeError."))
-			assert.True(t, strings.HasSuffix(line, "feature_test.go:90"))
+			assert.True(t, strings.HasSuffix(line, "future_test.go:90"))
 		}
 	}()
 	//
-	fea := NewFeature()
+	fea := NewFuture()
 	go func() {
 		defer func() {
 			if cause := recover(); cause != nil {
@@ -95,7 +95,7 @@ func TestFuture_CompleteError_RuntimeError(t *testing.T) {
 
 func TestFuture_Get(t *testing.T) {
 	run := false
-	fea := NewFeature()
+	fea := NewFuture()
 	go func() {
 		time.Sleep(500 * time.Millisecond)
 		run = true
