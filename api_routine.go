75dd4a4284bfe7526847aae125f256e7bbf0acfb
TimAndy <xuchonglei@126.com>
2023-05-22 11:46:43 +0800

    Support run a FutureTask and wrap a function to FutureTask instance

--- api_routine.go
+++ api_routine.go
@@ -11,16 +11,20 @@ type CancelRunnable func(token CancelToken)
 // CancelCallable provides a cancellable function that returns a value of type any.
 type CancelCallable func(token CancelToken) any
 
-// Go starts a new goroutine, and copy inheritableThreadLocals from current goroutine.
-// This function will auto invoke the fun and print error stack when panic occur in goroutine.
-func Go(fun Runnable) {
+// WrapTask create a new task and capture the inheritableThreadLocals from the current goroutine.
+// This function returns a FutureTask instance, but the return task will not run automatically.
+// You can run it in a sub-goroutine or goroutine-pool by FutureTask.Run method, wait by FutureTask.Get or FutureTask.GetWithTimeout method.
+// When the returned task run panic will be caught and error stack will be printed, the panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
+func WrapTask(fun Runnable) FutureTask {
 	// backup
 	copied := createInheritedMap()
-	go func() {
+	callable := func(task FutureTask) any {
 		// catch
 		defer func() {
 			if cause := recover(); cause != nil {
-				fmt.Println(NewRuntimeError(cause).Error())
+				err := NewRuntimeError(cause)
+				task.Fail(err)
+				fmt.Println(err.Error())
 			}
 		}()
 		// restore
@@ -35,27 +39,31 @@ func Go(fun Runnable) {
 				}
 			}()
 			fun()
+			return nil
 		} else {
-			backup := t.inheritableThreadLocals
+			threadLocalsBackup := t.threadLocals
+			inheritableThreadLocalsBackup := t.inheritableThreadLocals
 			defer func() {
-				t.threadLocals = nil
-				t.inheritableThreadLocals = backup
+				t.threadLocals = threadLocalsBackup
+				t.inheritableThreadLocals = inheritableThreadLocalsBackup
 			}()
 			t.threadLocals = nil
 			t.inheritableThreadLocals = copied
 			fun()
+			return nil
 		}
-	}()
+	}
+	return NewFutureTask(callable)
 }
 
-// GoWait starts a new goroutine, and copy inheritableThreadLocals from current goroutine.
-// This function return a FutureTask pointer, so we can wait by FutureTask.Get or FutureTask.GetWithTimeout method.
-// If panic occur in goroutine, The panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
-func GoWait(fun CancelRunnable) FutureTask {
-	task := NewFutureTask()
+// WrapWaitTask create a new task and capture the inheritableThreadLocals from the current goroutine.
+// This function returns a FutureTask instance, but the return task will not run automatically.
+// You can run it in a sub-goroutine or goroutine-pool by FutureTask.Run method, wait by FutureTask.Get or FutureTask.GetWithTimeout method.
+// When the returned task run panic will be caught, the panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
+func WrapWaitTask(fun CancelRunnable) FutureTask {
 	// backup
 	copied := createInheritedMap()
-	go func() {
+	callable := func(task FutureTask) any {
 		// catch
 		defer func() {
 			if cause := recover(); cause != nil {
@@ -74,58 +82,85 @@ func GoWait(fun CancelRunnable) FutureTask {
 				}
 			}()
 			fun(task)
-			task.Complete(nil)
+			return nil
 		} else {
-			backup := t.inheritableThreadLocals
+			threadLocalsBackup := t.threadLocals
+			inheritableThreadLocalsBackup := t.inheritableThreadLocals
 			defer func() {
-				t.threadLocals = nil
-				t.inheritableThreadLocals = backup
+				t.threadLocals = threadLocalsBackup
+				t.inheritableThreadLocals = inheritableThreadLocalsBackup
 			}()
 			t.threadLocals = nil
 			t.inheritableThreadLocals = copied
 			fun(task)
-			task.Complete(nil)
+			return nil
 		}
-	}()
+	}
+	return NewFutureTask(callable)
+}
+
+// WrapWaitResultTask create a new task and capture the inheritableThreadLocals from the current goroutine.
+// This function returns a FutureTask instance, but the return task will not run automatically.
+// You can run it in a sub-goroutine or goroutine-pool by FutureTask.Run method, wait and get result by FutureTask.Get or FutureTask.GetWithTimeout method.
+// When the returned task run panic will be caught, the panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
+func WrapWaitResultTask(fun CancelCallable) FutureTask {
+	// backup
+	copied := createInheritedMap()
+	callable := func(task FutureTask) any {
+		// catch
+		defer func() {
+			if cause := recover(); cause != nil {
+				task.Fail(NewRuntimeError(cause))
+			}
+		}()
+		// restore
+		t := currentThread(copied != nil)
+		if t == nil {
+			//copied is nil
+			defer func() {
+				t = currentThread(false)
+				if t != nil {
+					t.threadLocals = nil
+					t.inheritableThreadLocals = nil
+				}
+			}()
+			return fun(task)
+		} else {
+			threadLocalsBackup := t.threadLocals
+			inheritableThreadLocalsBackup := t.inheritableThreadLocals
+			defer func() {
+				t.threadLocals = threadLocalsBackup
+				t.inheritableThreadLocals = inheritableThreadLocalsBackup
+			}()
+			t.threadLocals = nil
+			t.inheritableThreadLocals = copied
+			return fun(task)
+		}
+	}
+	return NewFutureTask(callable)
+}
+
+// Go starts a new goroutine, and copy inheritableThreadLocals from current goroutine.
+// This function will auto invoke the func and print error stack when panic occur in goroutine.
+func Go(fun Runnable) {
+	task := WrapTask(fun)
+	go task.Run()
+}
+
+// GoWait starts a new goroutine, and copy inheritableThreadLocals from current goroutine.
+// This function will auto invoke the func and return a FutureTask instance, so we can wait by FutureTask.Get or FutureTask.GetWithTimeout method.
+// If panic occur in goroutine, The panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
+func GoWait(fun CancelRunnable) FutureTask {
+	task := WrapWaitTask(fun)
+	go task.Run()
 	return task
 }
 
 // GoWaitResult starts a new goroutine, and copy inheritableThreadLocals from current goroutine.
-// This function return a FutureTask pointer, so we can wait and get result by FutureTask.Get or FutureTask.GetWithTimeout method.
+// This function will auto invoke the func and return a FutureTask instance, so we can wait and get result by FutureTask.Get or FutureTask.GetWithTimeout method.
 // If panic occur in goroutine, The panic will be trigger again when calling FutureTask.Get or FutureTask.GetWithTimeout method.
 func GoWaitResult(fun CancelCallable) FutureTask {
-	task := NewFutureTask()
-	// backup
-	copied := createInheritedMap()
-	go func() {
-		// catch
-		defer func() {
-			if cause := recover(); cause != nil {
-				task.Fail(NewRuntimeError(cause))
-			}
-		}()
-		// restore
-		t := currentThread(copied != nil)
-		if t == nil {
-			//copied is nil
-			defer func() {
-				t = currentThread(false)
-				if t != nil {
-					t.threadLocals = nil
-					t.inheritableThreadLocals = nil
-				}
-			}()
-			task.Complete(fun(task))
-		} else {
-			backup := t.inheritableThreadLocals
-			defer func() {
-				t.threadLocals = nil
-				t.inheritableThreadLocals = backup
-			}()
-			t.threadLocals = nil
-			t.inheritableThreadLocals = copied
-			task.Complete(fun(task))
-		}
-	}()
+	task := WrapWaitResultTask(fun)
+	go task.Run()
 	return task
 }
