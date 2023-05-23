0bbb6823f86566ceac8a8d7c02288e6b2a64e47b
TimAndy <xuchonglei@126.com>
2023-05-23 20:20:48 +0800

    Add testcases for WrapTask

--- api_routine_test.go
+++ api_routine_test.go
@@ -3,6 +3,7 @@ package routine
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -41,6 +42,456 @@ func TestCancelCallable(t *testing.T) {
 	//
 	var fun func(CancelToken) any = cancelCallable
 	assert.Equal(t, "Hello", fun(nil))
+}
+
+func TestWrapTask_NoContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapTask(func() {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		task.Run()
+		assert.Nil(t, tls.Get())
+		assert.Nil(t, tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapTask_HasContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapTask(func() {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		tls.Set("你好")
+		tlsInherit.Set("inherit 你好")
+		task.Run()
+		assert.Equal(t, "你好", tls.Get())
+		assert.Equal(t, "inherit 你好", tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitTask_NoContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitTask(func(token CancelToken) {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		task.Run()
+		assert.Nil(t, tls.Get())
+		assert.Nil(t, tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	assert.Nil(t, task.Get())
+	assert.True(t, wrappedRun)
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitTask_NoContext_Cancel(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitTask(func(token CancelToken) {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		for i := 0; i < 1000; i++ {
+			if token.IsCanceled() {
+				return
+			}
+			time.Sleep(1 * time.Millisecond)
+		}
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		task.Run()
+		assert.Nil(t, tls.Get())
+		assert.Nil(t, tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	task.Cancel()
+	assert.True(t, task.IsCanceled())
+	assert.Panics(t, func() {
+		task.Get()
+	})
+	assert.False(t, wrappedRun)
+	wg.Wait()
+	assert.False(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitTask_HasContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitTask(func(token CancelToken) {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		tls.Set("你好")
+		tlsInherit.Set("inherit 你好")
+		task.Run()
+		assert.Equal(t, "你好", tls.Get())
+		assert.Equal(t, "inherit 你好", tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	assert.Nil(t, task.Get())
+	assert.True(t, wrappedRun)
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitTask_HasContext_Cancel(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitTask(func(token CancelToken) {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		for i := 0; i < 1000; i++ {
+			if token.IsCanceled() {
+				return
+			}
+			time.Sleep(1 * time.Millisecond)
+		}
+		wrappedRun = true
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		tls.Set("你好")
+		tlsInherit.Set("inherit 你好")
+		task.Run()
+		assert.Equal(t, "你好", tls.Get())
+		assert.Equal(t, "inherit 你好", tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	task.Cancel()
+	assert.True(t, task.IsCanceled())
+	assert.Panics(t, func() {
+		task.Get()
+	})
+	assert.False(t, wrappedRun)
+	wg.Wait()
+	assert.False(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitResultTask_NoContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitResultTask(func(token CancelToken) any {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+		return 1
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		task.Run()
+		assert.Nil(t, tls.Get())
+		assert.Nil(t, tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	assert.Equal(t, 1, task.Get())
+	assert.True(t, wrappedRun)
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitResultTask_NoContext_Cancel(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitResultTask(func(token CancelToken) any {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		for i := 0; i < 1000; i++ {
+			if token.IsCanceled() {
+				return 0
+			}
+			time.Sleep(1 * time.Millisecond)
+		}
+		wrappedRun = true
+		return 1
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		task.Run()
+		assert.Nil(t, tls.Get())
+		assert.Nil(t, tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	task.Cancel()
+	assert.True(t, task.IsCanceled())
+	assert.Panics(t, func() {
+		task.Get()
+	})
+	assert.False(t, wrappedRun)
+	wg.Wait()
+	assert.False(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitResultTask_HasContext(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitResultTask(func(token CancelToken) any {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		wrappedRun = true
+		return 1
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		tls.Set("你好")
+		tlsInherit.Set("inherit 你好")
+		task.Run()
+		assert.Equal(t, "你好", tls.Get())
+		assert.Equal(t, "inherit 你好", tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	assert.Equal(t, 1, task.Get())
+	assert.True(t, wrappedRun)
+	wg.Wait()
+	assert.True(t, wrappedRun)
+	assert.True(t, run)
+}
+
+func TestWrapWaitResultTask_HasContext_Cancel(t *testing.T) {
+	run := false
+	wrappedRun := false
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	tls := NewThreadLocal()
+	tlsInherit := NewInheritableThreadLocal()
+	tls.Set("hello")
+	tlsInherit.Set("inherit hello")
+	assert.Equal(t, "hello", tls.Get())
+	assert.Equal(t, "inherit hello", tlsInherit.Get())
+	task := WrapWaitResultTask(func(token CancelToken) any {
+		assert.Nil(t, tls.Get())
+		assert.Equal(t, "inherit hello", tlsInherit.Get())
+		tls.Set("世界")
+		tlsInherit.Set("inherit 世界")
+		assert.Equal(t, "世界", tls.Get())
+		assert.Equal(t, "inherit 世界", tlsInherit.Get())
+		for i := 0; i < 1000; i++ {
+			if token.IsCanceled() {
+				return 0
+			}
+			time.Sleep(1 * time.Millisecond)
+		}
+		wrappedRun = true
+		return 1
+	})
+	tls.Set("world")
+	tlsInherit.Set("inherit world")
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	go func() {
+		tls.Set("你好")
+		tlsInherit.Set("inherit 你好")
+		task.Run()
+		assert.Equal(t, "你好", tls.Get())
+		assert.Equal(t, "inherit 你好", tlsInherit.Get())
+		run = true
+		wg.Done()
+	}()
+	assert.Equal(t, "world", tls.Get())
+	assert.Equal(t, "inherit world", tlsInherit.Get())
+	task.Cancel()
+	assert.True(t, task.IsCanceled())
+	assert.Panics(t, func() {
+		task.Get()
+	})
+	assert.False(t, wrappedRun)
+	wg.Wait()
+	assert.False(t, wrappedRun)
+	assert.True(t, run)
 }
 
 func TestGo_Error(t *testing.T) {
